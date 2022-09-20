1a4313c34709873e77d1e1e92bb22abd1e870c0c
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2022-09-20 19:13:37 +0600

    dataset updated

--- webapp/views.py
+++ webapp/views.py
@@ -8,7 +8,7 @@ def home(request):
     row = RowTrack.objects.first()
     current_index = row.current_index
 
-    cell_img_url = 'B'+str(current_index)
+    cell_img_url = 'D'+str(current_index)
     img_url = ws[cell_img_url].value
 
     context = {
@@ -16,7 +16,7 @@ def home(request):
         'img_url': img_url
     }
     if request.method == 'POST':
-        cell_nsfw_category = 'I'+request.POST['row_index']
+        cell_nsfw_category = 'K'+request.POST['row_index']
         ws[cell_nsfw_category] = request.POST['radio-btn-input']
         wb.save('dataset/NSFW test 1.xlsx')
         row.current_index = int(request.POST['row_index'])+1
