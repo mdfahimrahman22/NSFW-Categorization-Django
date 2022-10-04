7cc7c7fd0cf5887a66e6a1fa2a55ca6d062e2439
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2022-10-05 01:54:02 +0600

    new category added

--- webapp/models.py
+++ webapp/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class RowTrack(models.Model):
     current_index = models.IntegerField(default=0)
+    pornography_count=models.IntegerField(default=0)
+    misogyny_count=models.IntegerField(default=0)
+    malignant_stereotypes_count=models.IntegerField(default=0)
+    malign_count=models.IntegerField(default=0)
+    delete_count=models.IntegerField(default=0)
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from webapp.models import RowTrack
 from core.settings import ws, wb
 # Create your views here.
@@ -11,19 +11,30 @@ def home(request):
     cell_img_url = 'D'+str(current_index)
     img_url = ws[cell_img_url].value
     cell_text = 'E'+str(current_index)
-    img_text=ws[cell_text].value
-    
+    img_text = ws[cell_text].value
+
     context = {
-        'row_index': current_index,
+        'row': row,
         'img_url': img_url,
-        'img_text':img_text
+        'img_text': img_text,
     }
     if request.method == 'POST':
         cell_nsfw_category = 'K'+request.POST['row_index']
+        category = request.POST['radio-btn-input']
         ws[cell_nsfw_category] = request.POST['radio-btn-input']
         wb.save('dataset/NSFW test 1.xlsx')
         row.current_index = int(request.POST['row_index'])+1
+        if category == 'Pornography':
+            row.pornography_count = row.pornography_count+1
+        elif category == 'Misogyny':
+            row.misogyny_count = row.misogyny_count+1
+        elif category == 'Malignant Stereotypes':
+            row.malignant_stereotypes_count = row.malignant_stereotypes_count+1
+        elif category == 'Malign':
+            row.malign_count = row.malign_count+1
+        elif category == 'Delete':
+            row.delete_count = row.delete_count+1
         row.save()
         return redirect('home')
-        
+
     return render(request, 'index.html', context)
